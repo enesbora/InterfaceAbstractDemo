aab2be10ab3ad5ccf2d928128329737d671efe52
enesbora <boraenes84@gmail.com>
2021-02-10 19:42:48 +0100

    Add project files.

---
+++ InterfaceAbstractDemo/Concrete/CustomerCheckManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class CustomerCheckManager:ICustomerCheckService
+    {
+        public object MernisServiceReference { get; private set; }
+
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            
+        }
+        
+    }
+}
+ 
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer{DateOfBirth = new DateTime(1984,3,2), FirstName = "Enes", LastName = "Bora", NationalityId = "12607745192"});
+        }
+    }
+}
